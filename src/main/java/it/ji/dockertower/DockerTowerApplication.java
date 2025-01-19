53f8635df16d3a010e140a083ce86f64d2eb7c36
sommovir <luca.coraci@gmail.com>
2025-01-19 16:54:13 +0100

    first commit

---
+++ src/main/java/it/ji/dockertower/DockerTowerApplication.java
@@ -0,0 +1,13 @@
+package it.ji.dockertower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTowerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/ji/dockertower/DockerTowerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ji.dockertower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
